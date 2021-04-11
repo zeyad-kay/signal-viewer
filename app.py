3c05d21c43ab947772322b488dd012382c0f133b
zeyad-khaled <45128084+zeyad-kay@users.noreply.github.com>
2021-04-11 12:50:02 +0200

    feat: switch to new tab

--- app.py
+++ app.py
@@ -5,36 +5,37 @@ from tab import Tab
 from menu import MenuBar
 
 class Application(tk.Frame):
-    def __init__(self,master=None):
+    def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.new_samples = {}
-        
         self.create_widgets()
 
     def create_widgets(self):
         """
         Create all initial components and listen for Fileupload event.
         """
-        self.master.grid_columnconfigure(0,weight=1)
-        self.master.grid_rowconfigure(0,weight=1)
+        self.master.grid_columnconfigure(0, weight=1)
+        self.master.grid_rowconfigure(0, weight=1)
         self.grid(sticky="nsew")
 
         self.master.config(menu=MenuBar(self))
 
         # Tabs container
-        self.columnconfigure(0,weight=1)
-        self.rowconfigure(0,weight=1)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
         self.tabs_container = ttk.Notebook(self)
-        self.tabs_container.grid(row=0,column=0,sticky="nsew")
-        
+        self.tabs_container.grid(row=0, column=0, sticky="nsew")
+
         # Start listening for file upload event
         # to create a new tab
-        self.master.bind("<<Fileupload>>",lambda e: self.create_tab(self.new_samples))
-    
-    def create_tab(self,samples):
-        tab = Tab(self.tabs_container,samples)
-        tab.columnconfigure(0,weight=1)
+        self.master.bind("<<Fileupload>>",
+                         lambda e: self.create_tab(self.new_samples))
+
+    def create_tab(self, samples):
+        tab = Tab(self.tabs_container, samples)
+        tab.columnconfigure(0, weight=1)
+        self.tabs_container.select(tab)
 
 if __name__ == "__main__":
     root = tk.Tk()
